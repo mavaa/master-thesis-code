e8e1ed76a6075d059a3534af5d8e782fc65ac444
Martin <1224973+mavaa@users.noreply.github.com>
2024-05-28 00:53:56 +0200

    r2runner takes a file string instead

--- src/r2runner.py
+++ src/r2runner.py
@@ -3,13 +3,11 @@ class R2Runner:
         self.subprocess = subprocess
         self.r2_path = r2_path
         self.r2_default_flags = r2_default_flags
-        pass
 
-    def run(self, command, executable_path, output, error=None):
-        pass
-
+    def run(self, command, executable_path, output_path, error=None):
         if error is None:
             error = self.subprocess.DEVNULL
 
-        self.subprocess.run([self.r2_path, f'-{self.r2_default_flags}', command, executable_path],
-                stdout=output, stderr=error, check=True)
+        with open(output_path, 'w') as output_file:
+            self.subprocess.run([self.r2_path, f'-{self.r2_default_flags}', command, executable_path],
+                                stdout=output_file, stderr=error, check=True)
--- tests/test_r2runner.py
+++ tests/test_r2runner.py
@@ -1,55 +1,63 @@
 import os
 import pytest
 import subprocess
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock, call, mock_open, patch
 from src.r2runner import R2Runner
 
 command = "aaa"
 test_sample_path = "tests/sample.o"
 
-def test_r2_run():
+@pytest.fixture
+def mock_open_fixture():
+    with patch("builtins.open", mock_open()) as mock_file:
+        yield mock_file
+
+def test_r2_run(mock_open_fixture):
     mock_subprocess = MagicMock()
     r2_runner = R2Runner(mock_subprocess)
 
-    output = MagicMock()
+    output_path = "output.txt"
     error = MagicMock()
 
-    r2_runner.run(command, test_sample_path, output, error)
+    r2_runner.run(command, test_sample_path, output_path, error)
 
+    mock_open_fixture.assert_called_once_with(output_path, 'w')
     mock_subprocess.run.assert_called_once_with(
             ['radare2', '-qc', command, test_sample_path],
-            stdout=output,
+            stdout=mock_open_fixture(),
             stderr=error,
             check=True)
 
-def test_r2_run_no_sdterr():
+def test_r2_run_no_stderr(mock_open_fixture):
     mock_subprocess = MagicMock()
     r2_runner = R2Runner(mock_subprocess)
 
-    output = MagicMock()
+    output_path = "output.txt"
 
-    r2_runner.run(command, test_sample_path, output)
+    r2_runner.run(command, test_sample_path, output_path)
 
+    mock_open_fixture.assert_called_once_with(output_path, 'w')
     mock_subprocess.run.assert_called_once_with(
             ['radare2', '-qc', command, test_sample_path],
-            stdout=output,
+            stdout=mock_open_fixture(),
             stderr=mock_subprocess.DEVNULL,
             check=True)
 
-def test_r2_run_custom():
+def test_r2_run_custom(mock_open_fixture):
     mock_subprocess = MagicMock()
     custom_path = "/usr/bin/r2"
     custom_flags = "abc"
     r2_runner = R2Runner(mock_subprocess, r2_path=custom_path, r2_default_flags=custom_flags)
 
-    output = MagicMock()
+    output_path = "output.txt"
     error = MagicMock()
 
-    r2_runner.run(command, test_sample_path, output, error)
+    r2_runner.run(command, test_sample_path, output_path, error)
 
+    mock_open_fixture.assert_called_once_with(output_path, 'w')
     mock_subprocess.run.assert_called_once_with(
-            [custom_path, f"-{custom_flags}", command, test_sample_path],
-            stdout=output,
+            [custom_path, f'-{custom_flags}', command, test_sample_path],
+            stdout=mock_open_fixture(),
             stderr=error,
             check=True)
 
@@ -57,6 +65,6 @@ def test_r2_run(tmp_path):
     r2_runner = R2Runner(subprocess)
     out_path = os.path.join(tmp_path, "test_out.txt")
 
-    r2_runner.run(command, test_sample_path, output=open(out_path, 'w'))
+    r2_runner.run(command, test_sample_path, out_path)
 
     assert os.path.exists(out_path), f"r2 output file ({out_path}) does not exist after r2 execution."
