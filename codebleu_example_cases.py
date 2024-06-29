d08581b34e7837013a95824c220f2a8dd675db72
Martin <1224973+mavaa@users.noreply.github.com>
2024-06-29 21:14:07 +0200

    codebleu_example use util

--- codebleu_example_cases.py
+++ codebleu_example_cases.py
@@ -1,20 +1,16 @@
 import os
 from codebleu import calc_codebleu
-
-def read_code_from_file(file_path):
-    with open(file_path, 'r') as file:
-        code = file.read()
-    return code
+from src.util import read_whole_file
 
 if __name__ == '__main__':
     base_path="data_codebleu_examples"
     reference_file=os.path.join(base_path, "reference.txt")
 
-    reference_code = read_code_from_file(reference_file)
+    reference_code = read_whole_file(reference_file)
 
     for i in range(4):
         filename=f'src{i+1}.txt'
-        prediction_code = read_code_from_file(os.path.join(base_path, filename))
+        prediction_code = read_whole_file(os.path.join(base_path, filename))
         bleu = calc_codebleu([reference_code], [prediction_code], lang="c", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=None)
 
 
