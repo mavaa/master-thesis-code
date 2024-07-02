023f0dbdad1cdcf2a9d140a4c40deede335c11de
Martin <1224973+mavaa@users.noreply.github.com>
2024-07-02 18:27:59 +0200

    Removed overly verbose comments

--- src/util.py
+++ src/util.py
@@ -14,24 +14,17 @@ def read_whole_file(file_path):
     return code
 
 def codebleu_create_graph(pkl_file_path, png_file_path):
-    # Load the data from the pickle file
     data = pd.read_pickle(pkl_file_path)
 
-    # Transform the dictionary into a dataframe for plotting
     df = pd.DataFrame(data)
-
-    # Resetting index for easier plotting
     df.reset_index(inplace=True)
     df = df.rename(columns={'index': 'Category'})
 
-    # Plotting
     plt.figure(figsize=(10, 6))
 
-    # Plot each algorithm's scores
-    for column in df.columns[1:]:  # Skipping the first column which is 'Category'
+    for column in df.columns[1:]:
         plt.plot(df['Category'], df[column], marker='o', label=column)
 
-    # Add titles and labels
     plt.title('CodeBLEU performance')
     plt.xlabel('Categories')
     plt.ylabel('Scores')
@@ -50,16 +43,9 @@ def codebleu_create_graph(pkl_file_path, png_file_path):
     ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: f'{int(y * 100)}%'))
     ax.yaxis.set_major_locator(MultipleLocator(0.05))
 
-    # Add a legend
     plt.legend()
-
-    # Add grid
     plt.grid(True)
-
-    # Save the plot to a file
     plt.savefig(png_file_path)
-
-    # Show the plot
     plt.show()
 
 def codebleu_create_latex_table(tex_file, results, result_keys, headers):
