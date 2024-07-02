f767903e66ac4780271539c113d529e90a857aad
Martin <1224973+mavaa@users.noreply.github.com>
2024-07-02 14:33:48 +0200

    Fixed name for r2 decompile predictor

--- src/predictor/r2decompilepredictor.py
+++ src/predictor/r2decompilepredictor.py
@@ -1,6 +1,7 @@
 class R2DecompilePredictor:
     def __init__(self, r2_runner):
         self.r2_runner = r2_runner
+        self.name = "R2_decompile"
 
     def generate_prediction(self, binary_path, disassembly_code):
         result, error = self.r2_runner.run_str("aaa;pdg", binary_path)
